a5dce17c44b3665fa52d892d6256c01128ffea4d
mzh5167 <202007655@stu.uob.edu.bh>
2022-12-27 13:36:16 +0300

    Added authorization

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth.admin' => \App\Http\Middleware\AuthAdmin::class,
+        'auth.customer' => \App\Http\Middleware\AuthCustomer::class,
+        'require.login' => \App\Http\Middleware\RequireLogin::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,89 @@ use App\Models\TimeSlot;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
+Route::get('/login', [LoginController::class, 'show'])
+  ->name("login");
+Route::post('/login', [LoginController::class, 'login']);
+
+Route::get('/register', [RegisterController::class, 'show']);
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/logout', function () {
+  session()->flush();
+  return redirect('login');
+});
+
+
+// Only accessible by admin
+Route::middleware(['auth.admin'])->group(function () {
+  Route::get('/add_movie', [MovieController::class, 'show_add_movie']);
+  Route::post('/add_movie', [MovieController::class, 'store']);
+
+  Route::get('/browse_movies', [MovieController::class, 'browse']);
+
+  Route::get('/edit_movie', [MovieController::class, 'show_edit']);
+  Route::post('/edit_movie', [MovieController::class, 'update']);
+
+
+  Route::get('/add_branch', [BranchController::class, 'show_add']);
+  Route::post('/add_branch', [BranchController::class, 'store']);
+
+  Route::get('/browse_branches', [BranchController::class, 'browse']);
+
+  Route::get('/edit_branch', [BranchController::class, 'show_edit']);
+  Route::post('/edit_branch', [BranchController::class, 'update']);
+
+
+  Route::get('/add_hall', [HallController::class, 'show_add']);
+  Route::post('/add_hall', [HallController::class, 'store']);
+
+  Route::get('/browse_halls', [HallController::class, 'browse']);
+
+  Route::get('/edit_hall', [HallController::class, 'show_edit']);
+  Route::post('/edit_hall', [HallController::class, 'update']);
+
+
+  Route::get('/add_time_slot', [TimeSlotController::class, 'show_add']);
+  Route::post('/add_time_slot', [TimeSlotController::class, 'store']);
+
+  Route::get('/browse_time_slots', [TimeSlotController::class, 'browse']);
+
+  Route::get('/edit_time_slot', [TimeSlotController::class, 'show_edit']);
+  Route::post('/edit_time_slot', [TimeSlotController::class, 'update']);
+
+  Route::get('/show_time_conflicts', [TimeSlotController::class, 'show_conflicts']);
+  Route::get('/show_seat_conflicts', [SeatController::class, 'show_conflicts']);
+
+  Route::get('/browse_bookings', [BookingController::class, 'browse']);
+
+
+  Route::get('/browse_seats', [SeatController::class, 'browse']);
+});
+
+
+// Only accessible by customer
+Route::middleware('auth.customer')->group(function () {
+  Route::get('/choose_seats', [SeatController::class, 'show_choose']);
+  Route::post('/choose_seats', [SeatController::class, 'receive_chosen_seats']);
+
+  Route::get('/choose_time_slot', [TimeSlotController::class, 'show_choose']);
+  Route::get('/confirm_booking', [BookingController::class, 'confirm_booking']);
+});
+
+
+// Accessible by both admin and customer
+Route::middleware('require.login')->group(function () {
+  Route::get('/movie_details', [MovieController::class, 'show']);
+
+  Route::get('/', function () {
+    return session("isAdmin") ?
+      view('home.admin') :
+      view('home.customer');
+  })->name('home');
+});
+
+
 
 Route::match(["get", "post"], '/test', function (Request $request) {
   // $request->session()->flush();
@@ -42,95 +122,15 @@ Route::match(["get", "post"], '/test', function (Request $request) {
   );
 });
 
-Route::get('/login', [LoginController::class, 'show'])
-  ->name("login");
-// Route::get('/login', function () { return (new LoginController())->show(); } );
-Route::post('/login', [LoginController::class, 'login']);
-// Route::post('/login', 'LoginController@login');
-// Route::post('/login', function(Request $request) {
-//     print_r($request->all());
-//     die();
-// });
-
-Route::resource('customer', CustomerController::class)
-  ->only(['index', 'store']);
-
-
-Route::view('test_show_user_template', 'show_user');
-
-Route::get('/register', [RegisterController::class, 'show']);
-Route::post('/register', [RegisterController::class, 'register']);
-
-Route::get('/add_movie', [MovieController::class, 'show_add_movie']);
-Route::post('/add_movie', [MovieController::class, 'store']);
-
-Route::get('/movie_details', [MovieController::class, 'show']);
-
-Route::get('/browse_movies', [MovieController::class, 'browse']);
-
-Route::get('/add_branch', [BranchController::class, 'show_add']);
-Route::post('/add_branch', [BranchController::class, 'store']);
-
-Route::get('/browse_branches', [BranchController::class, 'browse']);
-
-Route::get('/', function () {
-  if (session()->has('isAdmin')) {
-    return session("isAdmin") ?
-      view('home.admin') :
-      view('home.customer');
-  } else {
-    return redirect('login');
-  }
-})
-  ->name('home');
 
 Route::any('view_session', function () {
   dd(session()->all());
 });
 
-Route::get('/logout', function () {
-  session()->flush();
-  return redirect('login');
-});
-
-
-Route::get('/add_hall', [HallController::class, 'show_add']);
-Route::post('/add_hall', [HallController::class, 'store']);
-
-Route::get('/browse_halls', [HallController::class, 'browse']);
-
-
-Route::get('/add_time_slot', [TimeSlotController::class, 'show_add']);
-Route::post('/add_time_slot', [TimeSlotController::class, 'store']);
-
-Route::get('/browse_time_slots', [TimeSlotController::class, 'browse']);
-Route::get('/show_time_conflicts', [TimeSlotController::class, 'show_conflicts']);
-
-
-Route::get('/browse_bookings', [BookingController::class, 'browse']);
-Route::get('/show_seat_conflicts', [SeatController::class, 'show_conflicts']);
 
 // TODO: remove.  These are only intended for testing
 Route::get('/add_booking', [BookingController::class, 'show_add']);
 Route::post('/add_booking', [BookingController::class, 'store']);
-
-Route::get('/browse_seats', [SeatController::class, 'browse']);
-Route::get('/choose_seats', [SeatController::class, 'show_choose']);
-Route::post('/choose_seats', [SeatController::class, 'receive_chosen_seats']);
-
-Route::get('/edit_movie', [MovieController::class, 'show_edit']);
-Route::post('/edit_movie', [MovieController::class, 'update']);
-Route::get('/edit_branch', [BranchController::class, 'show_edit']);
-Route::post('/edit_branch', [BranchController::class, 'update']);
-Route::get('/edit_hall', [HallController::class, 'show_edit']);
-Route::post('/edit_hall', [HallController::class, 'update']);
-Route::get('/edit_time_slot', [TimeSlotController::class, 'show_edit']);
-Route::post('/edit_time_slot', [TimeSlotController::class, 'update']);
-
-
-
-Route::get('/choose_time_slot', [TimeSlotController::class, 'show_choose']);
-Route::get('/confirm_booking', [BookingController::class, 'confirm_booking']);
 
 
 Route::get('/test', [TimeSlotController::class, 'tmp']);
