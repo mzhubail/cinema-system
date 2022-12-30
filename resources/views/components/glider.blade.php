adbef10b20f7a5f38134b5343075a99c941f0add
mzh5167 <202007655@stu.uob.edu.bh>
2022-12-30 12:53:44 +0300

    Moved glider into a custom component

---
+++ resources/views/components/glider.blade.php
@@ -0,0 +1,67 @@
+<div class="glider-contain">
+  <div class="glider">
+    {{ $slot }}
+  </div>
+
+  <button aria-label="Previous" class="glider-prev">«</button>
+  <button aria-label="Next" class="glider-next">»</button>
+  <div role="tablist" class="dots"></div>
+</div>
+
+
+@push('scripts')
+  <script src="assets/glider.js"></script>
+  <script>
+    window.addEventListener('load', function() {
+      new Glider(document.querySelector('.glider'), {
+        // slidesToShow: 3,
+        // dots: '#dots',
+        // arrows: {
+        //   prev: '.glider-prev',
+        //   next: '.glider-next'
+        // }
+
+        // Mobile-first defaults
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        scrollLock: true,
+        dots: '#resp-dots',
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        },
+        // rewind: true,
+        responsive: [{
+          // screens greater than >= 576px
+          breakpoint: 576,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 2, //'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+          }
+        }, {
+          // screens greater than >= 768px
+          breakpoint: 768,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: 3, //'auto',
+            slidesToScroll: 'auto',
+            itemWidth: 150,
+            duration: 0.25
+          }
+        }, {
+          // screens greater than >= 992px
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            itemWidth: 150,
+            duration: 0.25
+          }
+        }]
+      })
+    })
+  </script>
+@endpush
--- resources/views/home/customer.blade.php
+++ resources/views/home/customer.blade.php
@@ -3,23 +3,16 @@
 @section('title', 'Home')
 
 @section('main')
-  <div class="glider-contain">
-    <div class="glider">
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-      <x-movie-card />
-    </div>
-
-    <button aria-label="Previous" class="glider-prev">«</button>
-    <button aria-label="Next" class="glider-next">»</button>
-    <div role="tablist" class="dots"></div>
-  </div>
-
+  <x-glider>
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+    <x-movie-card />
+  </x-glider>
   <x-movie-cards-container>
     <x-movie-card />
     <x-movie-card />
@@ -31,61 +24,4 @@
 
 @push('styles')
   <link rel="stylesheet" type="text/css" href="assets/glider.css">
-  <script src="assets/glider.js"></script>
-@endpush
-
-@push('scripts')
-  <script>
-    window.addEventListener('load', function() {
-      new Glider(document.querySelector('.glider'), {
-        // slidesToShow: 3,
-        // dots: '#dots',
-        // arrows: {
-        //   prev: '.glider-prev',
-        //   next: '.glider-next'
-        // }
-
-        // Mobile-first defaults
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        scrollLock: true,
-        dots: '#resp-dots',
-        arrows: {
-          prev: '.glider-prev',
-          next: '.glider-next'
-        },
-        // rewind: true,
-        responsive: [{
-          // screens greater than >= 576px
-          breakpoint: 576,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 2, //'auto',
-            slidesToScroll: 'auto',
-            itemWidth: 150,
-            duration: 0.25
-          }
-        }, {
-          // screens greater than >= 768px
-          breakpoint: 768,
-          settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-            slidesToShow: 3, //'auto',
-            slidesToScroll: 'auto',
-            itemWidth: 150,
-            duration: 0.25
-          }
-        }, {
-          // screens greater than >= 992px
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            itemWidth: 150,
-            duration: 0.25
-          }
-        }]
-      })
-    })
-  </script>
 @endpush
