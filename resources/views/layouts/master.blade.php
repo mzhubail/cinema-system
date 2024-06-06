a16f941383ae3875af73473057c20eb0491dd378
mzhubail <mzhubail@gmail.com>
2024-06-06 14:18:49 +0300

    Add custom bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,8 +11,7 @@
   <!-- <link href="/static/favicon.ico" rel="icon"> -->
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-    integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+  <link rel="stylesheet" href="/assets/bootstrap.min.css">
   <!-- <link rel="stylesheet" href="../custom.css"> -->
 
   <!-- Bootstrap Icons -->
@@ -152,8 +151,7 @@ Data provided for free by <a href="https://iextrading.com/developer">IEX</a>. Vi
     integrity="sha384-i61gTtaoovXtAbKjo903+O55Jkn2+RtzHtvNez+yI49HAASvznhe9sZyjaSHTau9" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
     integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
-    integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+  <script src="assets/bootstrap.min.js"></script>
   <script src="assets/script.js"></script>
 
   @stack('scripts')
