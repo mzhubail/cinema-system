f56dcea268ffbfaca129516abd1473b057afdc66
mzhubail <mzhubail@gmail.com>
2024-06-06 16:33:22 +0300

    Rework admin page design

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -37,41 +37,72 @@
 @section('main')
   <div class="row">
     <div class="col-md-4">
-      <h3 class="mt-3">Branches</h3>
-      <a class="text-decoration-none" href="/browse_branches"> Browse Branches </a> <br>
-      <a class="text-decoration-none" href="/add_branch"> Add Branch </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-buildings" viewBox="0 0 16 16">
+          <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022M6 8.694 1 10.36V15h5zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5z"/>
+          <path d="M2 11h1v1H2zm2 0h1v1H4zm-2 2h1v1H2zm2 0h1v1H4zm4-4h1v1H8zm2 0h1v1h-1zm-2 2h1v1H8zm2 0h1v1h-1zm2-2h1v1h-1zm0 2h1v1h-1zM8 7h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zM8 5h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zm0-2h1v1h-1z"/>
+        </svg>
+        Branches
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_branches"> Browse Branches </a> <br>
+      <a class="text-decoration-none text-dark" href="/add_branch"> Add Branch </a>
 
-      <h3 class="mt-3">Halls</h3>
-      <a class="text-decoration-none" href="/browse_halls"> Browse Halls </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-building" viewBox="0 0 16 16">
+          <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
+          <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3z"/>
+        </svg>
+        Halls
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_halls"> Browse Halls </a>
       <br>
-      <a class="text-decoration-none" href="/add_hall"> Add Hall </a>
+      <a class="text-decoration-none text-dark" href="/add_hall"> Add Hall </a>
 
     </div>
 
     <div class="col-md-4">
-      <h3 class="mt-3">Movies</h3>
-      <a class="text-decoration-none" href="/browse_movies"> Browse Movies </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-film" viewBox="0 0 16 16">
+          <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+        </svg>
+        Movies
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_movies"> Browse Movies </a>
       <br>
-      <a class="text-decoration-none" href="/add_movie"> Add Movie </a>
+      <a class="text-decoration-none text-dark" href="/add_movie"> Add Movie </a>
 
-      <h3 class="mt-3">Seats</h3>
-      <a class="text-decoration-none" href="/browse_seats"> Show Seats </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"/><line x1="112" y1="232" x2="216" y2="232" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path d="M104.84,76.42a8,8,0,0,0,0,7.16l31.78,64a8,8,0,0,0,7.16,4.42H208a8,8,0,0,1,8,8v32a8,8,0,0,1-8,8H114.11a8,8,0,0,1-7.15-4.42l-58.12-116a8,8,0,0,1,0-7.16L71,28.42a8,8,0,0,1,10.73-3.57l33.89,14.31a8,8,0,0,1,3.57,10.73Z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/></svg>
+        Seats
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_seats"> Show Seats </a>
       <br>
-      <a class="text-decoration-none" href="/show_seat_conflicts"> Show Seat conflicts </a>
+      <a class="text-decoration-none text-dark" href="/show_seat_conflicts"> Show Seat conflicts </a>
     </div>
 
     <div class="col-md-4">
-      <h3 class="mt-3">Booking</h3>
-      <a class="text-decoration-none" href="/browse_bookings_by_customer"> Browse Bookings by customer </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-ticket" viewBox="0 0 16 16">
+          <path d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5V6a.5.5 0 0 1-.5.5 1.5 1.5 0 0 0 0 3 .5.5 0 0 1 .5.5v1.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 11.5V10a.5.5 0 0 1 .5-.5 1.5 1.5 0 1 0 0-3A.5.5 0 0 1 0 6zM1.5 4a.5.5 0 0 0-.5.5v1.05a2.5 2.5 0 0 1 0 4.9v1.05a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-1.05a2.5 2.5 0 0 1 0-4.9V4.5a.5.5 0 0 0-.5-.5z"/>
+        </svg>
+        Booking
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_bookings_by_customer"> Browse Bookings by customer </a>
       <br>
-      <a class="text-decoration-none" href="/browse_bookings_by_time_slot"> Browse Bookings by time slot </a>
+      <a class="text-decoration-none text-dark" href="/browse_bookings_by_time_slot"> Browse Bookings by time slot </a>
 
-      <h3 class="mt-3">Time Slots</h3>
-      <a class="text-decoration-none" href="/browse_time_slots"> Browse Time Slots </a>
+      <h3 class="mt-3 text-primary font-weight-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-clock" viewBox="0 0 16 16">
+          <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
+          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0"/>
+        </svg>
+        Time Slots
+      </h3>
+      <a class="text-decoration-none text-dark" href="/browse_time_slots"> Browse Time Slots </a>
       <br>
-      <a class="text-decoration-none" href="/add_time_slot"> Add Time Slot </a>
+      <a class="text-decoration-none text-dark" href="/add_time_slot"> Add Time Slot </a>
       <br>
-      <a class="text-decoration-none" href="/show_time_conflicts"> Show Time Slot conflicts </a>
+      <a class="text-decoration-none text-dark" href="/show_time_conflicts"> Show Time Slot conflicts </a>
     </div>
   </div>
 
